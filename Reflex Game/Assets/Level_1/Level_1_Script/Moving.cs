c518b4d9833157b4891644d63c9f96d76f487fa9
Arhenon <gagatyn2017@inbox.ru>
2024-04-02 13:33:55 +0300

    Revert "drag and drop 0.15"
    This reverts commit 6a984111d2934558a95861c0f772d21dbb8f250e.

--- Reflex Game/Assets/Level_1/Level_1_Script/Moving.cs
+++ Reflex Game/Assets/Level_1/Level_1_Script/Moving.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 public class Moving : MonoBehaviour
 {
     bool move;
-    Vector3 mousePos;
+    Vector2 mousePos;
     float startPosX;
     float startPosY;
-    //Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
     public GameObject form1;
  
@@ -20,7 +19,7 @@ public class Moving : MonoBehaviour
 
         if (((Mathf.Abs(this.transform.localPosition.x - form1.transform.localPosition.x))<=5 && (Mathf.Abs(this.transform.localPosition.y - form1.transform.localPosition.y)) <= 5 ))
         { 
-            this.transform.position = new Vector3(form1.transform.position.x, form1.transform.position.y);
+            this.transform.position = new Vector2(form1.transform.position.x, form1.transform.position.y);
 
             finish = true;
         }
@@ -44,11 +43,8 @@ public class Moving : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (move)
-        {
-            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); ;
-            this.gameObject.transform.localPosition = mousePos*5/2;
-        }//new Vector3((mousePos.x - startPosX), (mousePos.y - startPosY)); }
+        if (move) { mousePos = Input.mousePosition;  
+            this.gameObject.transform.localPosition = new Vector2((mousePos.x - startPosX)/10, (mousePos.y - startPosY))/10; }
 
     }
 }
