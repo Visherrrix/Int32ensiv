c3bba3371e0c04f4b0ac4e6873cb40b3996716ec
Denis Egerew <egereffden@gmail.com>
2024-04-02 17:20:14 +0300

    drag and drop 0.3
    Карточки перемещаются как в нормальном drag and drop. В ячейки по не встают

--- Reflex Game/Assets/Level_1/Level_1_Script/Moving.cs
+++ Reflex Game/Assets/Level_1/Level_1_Script/Moving.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Moving : MonoBehaviour
 {
     bool move;
-    Vector3 mousePos;
+    private Vector3 pointScreen;
+    private Vector3 offset;
+    //Vector3 mousePos;
     float startPosX;
     float startPosY;
     //Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -14,27 +16,39 @@ public class Moving : MonoBehaviour
  
     bool finish;
 
-    private void OnMouseUp()
+    private void OnMouseDown()
     {
-        move = false;
-
-        if (((Mathf.Abs(this.transform.localPosition.x - form1.transform.localPosition.x))<=5 && (Mathf.Abs(this.transform.localPosition.y - form1.transform.localPosition.y)) <= 5 ))
-        { 
-            this.transform.position = new Vector3(form1.transform.position.x, form1.transform.position.y);
-
-            finish = true;
-        }
-        
+        pointScreen = Camera.main.ScreenToWorldPoint(gameObject.transform.position);
+        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, pointScreen.z));
     }
 
-    void OnMouseDown()
+    private void OnMouseDrag()
     {
-        if(Input.GetMouseButtonDown(0))
-        move = true;
-        mousePos = Input.mousePosition;
-        startPosX = mousePos.x - this.transform.localPosition.x ;
-        startPosY = mousePos.y - this.transform.localPosition.y;
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, pointScreen.z);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+        transform.position = curPosition;
     }
+    //private void OnMouseUp()
+    //{
+    //    move = false;
+
+    //    if (((Mathf.Abs(this.transform.localPosition.x - form1.transform.localPosition.x)) <= 5 && (Mathf.Abs(this.transform.localPosition.y - form1.transform.localPosition.y)) <= 5))
+    //    {
+    //        this.transform.position = new Vector3(form1.transform.position.x, form1.transform.position.y);
+
+    //        finish = true;
+    //    }
+
+    //}
+
+    //void OnMouseDown()
+    //{
+    //    if(Input.GetMouseButtonDown(0))
+    //    move = true;
+    //    mousePos = Input.mousePosition;
+    //    startPosX = mousePos.x - this.transform.localPosition.x ;
+    //    startPosY = mousePos.y - this.transform.localPosition.y;
+    //}
     // Start is called before the first frame update
     void Start()
     {
@@ -44,11 +58,11 @@ public class Moving : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (move)
-        {
-            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); ;
-            this.gameObject.transform.localPosition = mousePos*5/2;
-        }//new Vector3((mousePos.x - startPosX), (mousePos.y - startPosY)); }
+        //if (move)
+        //{
+        //    mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition,Camera.main.nearClipPlane); ;
+        //    this.gameObject.transform.localPosition = mousePos*4;
+        //}//new Vector3((mousePos.x - startPosX), (mousePos.y - startPosY)); }
 
     }
 }
