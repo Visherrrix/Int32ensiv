24dfea80eed3278b3434a90539393fb26094d18b
Denis Egerew <egereffden@gmail.com>
2024-04-16 15:52:31 +0300

    Fix

--- Reflex Game/Assets/Scenes/Collection/scripts/ManMono.cs
+++ Reflex Game/Assets/Scenes/Collection/scripts/ManMono.cs
@@ -16,7 +16,7 @@ public class ManMono : MonoBehaviour
     }
 
     public GameObject Gal1, Gal2, Gal3, Gal4, Gal5, Gal6;
-    public static bool[] ArrGal = { false, false, false, false, false, false, false, false };
+    public static bool[] ArrGal = { false, false, false, false, false, false, false, false,false };
     public static bool hf = false;
     // Start is called before the first frame update
     
@@ -29,19 +29,19 @@ public class ManMono : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        for (int i = 0; i < 8; i++)
+        for (int i = 1; i < 9; i++)
         {
             //Debug.Log(ArrGal[i]);
             if (ArrGal[i] == true && hf)
             {
                 
                 
-                if (i == 0) { Renderer a = Gal1.GetComponent<Renderer>(); a.enabled = true; }
-                if (i == 7) { Renderer b = Gal6.GetComponent<Renderer>(); b.enabled = true; }
-                if (i == 6) { Renderer c = Gal5.GetComponent<Renderer>(); c.enabled = true; }
-                if (i == 3) { Renderer d = Gal2.GetComponent<Renderer>(); d.enabled = true; }
-                if (i == 4) { Renderer e = Gal3.GetComponent<Renderer>(); e.enabled = true; }
-                if (i == 5) { Renderer f = Gal4.GetComponent<Renderer>(); f.enabled = true; }
+                if (i == 1) { Renderer a = Gal1.GetComponent<Renderer>(); a.enabled = true; }
+                if (i == 8) { Renderer b = Gal6.GetComponent<Renderer>(); b.enabled = true; }
+                if (i == 7) { Renderer c = Gal5.GetComponent<Renderer>(); c.enabled = true; }
+                if (i == 4) { Renderer d = Gal2.GetComponent<Renderer>(); d.enabled = true; }
+                if (i == 5) { Renderer e = Gal3.GetComponent<Renderer>(); e.enabled = true; }
+                if (i == 6) { Renderer f = Gal4.GetComponent<Renderer>(); f.enabled = true; }
 
             }
         }
